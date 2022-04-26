3563e215171376daab984740a11662367e1cdb2c
Michael-Li-666 <yuangli@brandeis.edu>
2022-04-26 16:00:20 -0400

    add git ignore

--- app.js
+++ app.js
@@ -100,64 +100,68 @@ app.get("/:customListName", function(req, res) {
 //add item to list
 app.post("/", function(req, res) {
 
-      const itemName = req.body.newItem;
-      const listName = req.body.list;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-      const item = new Item({
-        name: itemName
-      });
-      if (listName === "Today") {
-        item.save();
-        res.redirect("/")
-        }
-        else {
-          List.findOne({
-            name: listName
-          }, function(err, foundList) {
-            foundList.items.push(item);
-            foundList.save();
-            res.redirect("/" + listName);
-          });
-        }
-      });
+  const item = new Item({
+    name: itemName
+  });
+  if (listName === "Today") {
+    item.save();
+    res.redirect("/")
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
 
-    //delete item from list
-    app.post("/delete", function(req, res) {
-      const checkedItemId = req.body.checkbox;
-      const listName = req.body.listName;
+//delete item from list
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
 
-      if (listName === "Today") {
-        Item.findByIdAndRemove(checkedItemId, function(err) {
-          if (!err) {
-            console.log("Successfully deleted checked item.");
-            res.redirect("/");
-          }
-        });
-      } else {
-        List.findOneAndUpdate({
-          name: listName
-        }, {
-          $pull: {
-            items: {
-              _id: checkedItemId
-            }
-          }
-        }, function(err, foundList) {
-          if (!err) {
-            res.redirect("/" + listName);
-          }
-        });
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err) {
+      if (!err) {
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
       }
-
-
     });
-
-    //about page
-    app.get("/about", function(req, res) {
-      res.render("about");
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
+        res.redirect("/" + listName);
+      }
     });
+  }
 
-    //server
-    app.listen(3000, function() {
-      console.log("Server started on port 3000");
-    });
+
+});
+
+//about page
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+//server
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("server running")
+
+});
